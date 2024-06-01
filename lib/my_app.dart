728f940ec3dadc34dd78956b7a09276b4955d306
abdo1917 <102588811+abdo1917@users.noreply.github.com>
2024-06-01 21:20:48 +0300

    init app and install packages

---
+++ lib/my_app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tododb/view/splash_screen/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'ToDo',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/view_mode/themes/light_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tododb/view_mode/utils/app_colors.dart';
+
+ThemeData lightTheme = ThemeData(
+  primaryColor: AppColors.purple,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: AppColors.purple,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          12.r,
+        ),
+      ),
+    ),
+  ),
+);
