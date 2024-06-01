60e541c32c9c92538d46ff49699e1721022861c2
abdo1917 <102588811+abdo1917@users.noreply.github.com>
2024-06-01 21:26:21 +0300

    install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,82 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:tododb/firebase_options.dart';
+import 'package:tododb/my_app.dart';
+import 'package:tododb/view_mode/data/local/shared_helper.dart';
+import 'package:tododb/view_mode/utils/app_assets.dart';
 
-void main() {
-  runApp(const MyApp());
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await SharedHelper.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false,
+    criticalAlert: true,
+    provisional: true,
+    sound: true,
+  );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  print('User granted permission: ${settings.authorizationStatus}');
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+    final player = AudioPlayer();
+    final duration = await player.setAsset(AppAssets.error);
+    player.play();
 
-  final String title;
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification?.toMap().toString()}');
+    }
+  });
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final fcmToken = await FirebaseMessaging.instance.getToken();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  FirebaseMessaging.instance.onTokenRefresh
+      .listen((fcmToken) {
+    // TODO: If necessary send token to application server.
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    // Note: This callback is fired at each app startup and whenever a new
+    // token is generated.
+  })
+      .onError((err) {
+    // Error getting token.
+  });
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      path: 'assets/translations',
+      // <-- change the path of the translation files
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+    ),
+  );
 }
---
+++ lib/translation/locale_keys.g.dart
@@ -0,0 +1,42 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const login = 'login';
+  static const email = 'email';
+  static const emailError = 'emailError';
+  static const name = 'name';
+  static const nameError = 'nameError';
+  static const password = 'password';
+  static const passwordError = 'passwordError';
+  static const password_confirmation = 'password_confirmation';
+  static const password_confirmationError = 'password_confirmationError';
+  static const doNotHaveAnAccount = 'doNotHaveAnAccount';
+  static const register = 'register';
+  static const todoApp = 'todoApp';
+  static const addTask = 'addTask';
+  static const title = 'title';
+  static const titleError = 'titleError';
+  static const description = 'description';
+  static const descriptionError = 'descriptionError';
+  static const startDate = 'startDate';
+  static const startDateError = 'startDateError';
+  static const endDate = 'endDate';
+  static const endDateError = 'endDateError';
+  static const image = 'image';
+  static const status = 'status';
+  static const add = 'add';
+  static const edit = 'edit';
+  static const save = 'save';
+  static const delete = 'delete';
+  static const deleteConfirmation = 'deleteConfirmation';
+  static const yes = 'yes';
+  static const no = 'no';
+  static const addPhotoToYourNote = 'addPhotoToYourNote';
+  static const authError = 'authError';
+  static const noMoreDataToLoad = 'noMoreDataToLoad';
+  static const registerSuccessfully = 'registerSuccessfully';
+  static const loginSuccessfully = 'loginSuccessfully';
+  static const forgetPassword = 'forgetPassword';
+  static const editTask = 'editTask';
+
+}
